802ecc3265004a0d894c5a6231b6c5c002c72a6d
owen <stensonowen@gmail.com>
2016-06-27 01:13:11 -0400

    basic starter misc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,33 @@
+use std::collections::HashSet;
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+
+impl Point {
+    fn get_neighbors(&self) -> Vec<Point> {
+        [(-1, 1), ( 0, 1), ( 1, 1),
+         (-1, 0),          ( 1, 0),
+         (-1,-1), ( 0,-1), ( 1,-1),]
+             .iter()
+             .map(|&(dx,dy)| Point{x: self.x+dx, y: self.y+dy})
+             .collect()
+    }
+}
+
+
+//type Board = std::collections::HashSet<Point>;
+struct Board {
+    cells: HashSet<Point>,
+}
+
+impl Board {
+
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
